42e6e838cfcccd20035f6dbf1fedba74b78aaad4
vinibispo <vini.bispo015@gmail.com>
2020-03-14 08:48:02 -0300

    Dois testes que estavam quebrando agora funciona

--- spec/controller/adm_controller_spec.js
+++ spec/controller/adm_controller_spec.js
@@ -5,9 +5,8 @@ const Adm = require('../../app/models/adm');
 
 describe("AdmController", () => {
   beforeEach(async()=>{
-    Adm.deleteMany()
-    const adm = await Adm.create([{ nome: "Danilo1", senha: '123456', email: 'danilo1@torneseumprogramador.com.br' },{ nome: "Danilo2", senha: '123456', email: 'danilo2@torneseumprogramador.com.br' }])
-    
+    await Adm.deleteMany()
+    await Adm.create([{ nome: "Danilo1", senha: '123456', email: 'danilo1@torneseumprogramador.com.br' },{ nome: "Danilo2", senha: '123456', email: 'danilo2@torneseumprogramador.com.br' }])
     })
   describe("GET /adm.json - deve retornar uma lista de administradores", () => {
     it("deve retornar o status code de 200", async(done) => {
@@ -16,33 +15,25 @@ describe("AdmController", () => {
       done();
     });
 
-    it("deve dados na API", (done) => {
-      axios(options).then(response => {
-        // itens = JSON.parse(response.body);
-        // expect(itens[0].nome).toBe("Danilo1");
-        // expect(itens[1].nome).toBe("Danilo2");
-      // })
-      // .catch(function (err) {
-        // throw err.message
-      // });
-      // done();
-    // });
+    it("deve dados na API", async(done) => {
+       const response = await axios.get(`${host}/adm.json`)
+        const itens = response.data;
+        expect(itens[0].nome).toBe("Danilo1");
+        expect(itens[1].nome).toBe("Danilo2");
+        done()
+      })
   });
 // 
 
-  // describe("POST /adm.json - administrador", () => {
-    // it("deve cadastrar um administrador", (done) => {
-      // let nome = `teste ${new Date().getTime()}`;
-      // let options = {
-        // method: 'POST',
-        // uri: `${host}/adm.json`,
-        // body: { 
-          // nome: nome,
-          // senha: '123456',
-          // email: nome + '@torneseumprogramador.com.br' 
-        // },
-        // json: true
-      // };
+  describe("POST /adm.json - administrador", () => {
+    it("deve cadastrar um administrador", async(done) => {
+      let nome = `teste ${new Date().getTime()}`;
+        const body = { 
+          nome,
+          senha: '123456',
+          email: nome + '@torneseumprogramador.com.br' 
+        },
+        const rsponse = await axios.post(`${host}/adm.json`)
       // axios(options).then(response => {
         // expect(response.statusCode).toBe(201);
       // })
